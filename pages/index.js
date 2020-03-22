1b7ee759335a2a2ad92fa521acb4d5c6df35c877
Kamogelo <kamogeloCharmaine142@gmail.com>
2020-03-22 13:04:06 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,8 @@ export default function Index() {
       
     <div>
     <Head></Head>
-      <div className="devhub-w-full devhub-bg-black devhub-h-screen devhub-justify-center devhub-items-center  devhub-flex">
-        <div className="devhub-flex-wrap">
-          <p className="devhub-text-white">Page under contruction</p>
-        </div>
-        
-      </div>
+      <div style="margin-top:350px; margin-left:725px;"><a href="https://github.com/login/oauth/authorize?client_id=bf37cd3a98ec77d66a8d"> <i class="fa fa-github" style="font-size:100px;color:white"></i></a></div>
+      <div><img src="https://github.com/GreyWolfLab/devPortal/public/images/devHub01.png" style="height:50px; width:50px; margin-top:225px; margin-left: 25px;"></div>
 
       </div>
     );
