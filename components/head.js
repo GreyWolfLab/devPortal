2de06ed00c9d3a2cc21f7521f4b14e37a4884e4c
Kamogelo <kamogeloCharmaine142@gmail.com>
2020-03-22 12:59:38 +0200

    Update head.js

--- components/head.js
+++ components/head.js
@@ -22,6 +22,7 @@ const Head = props => (
     <link type="text/css" rel="stylesheet" href="css/styles.css" />
     <link type="text/css" rel="stylesheet" href="css/devhubza.css" />
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
     <meta property="og:url" content={props.url || defaultOGURL} />
