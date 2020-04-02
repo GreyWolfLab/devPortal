a3519b38be5bab89e3bf5eb0842ed753e8df68f6
rawkthethinker <47385943+rawkthethinker@users.noreply.github.com>
2020-04-02 09:35:27 +0200

    fixed logo responsiveness

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,13 @@ export default function Index() {
       
     <div>
     <Head></Head>
-      <div className="devhub-w-screen devhub-h-screen devhub-flex devhub-items-center devhub-justify-center devhub-bg-black">
+      <div className="devhub-w-screen devhub-h-screen devhub-flex devhub-items-end  devhub-bg-black">
         
         
         <div className="devhub-flex-wrap">
           <div className="devhub-flex devhub-h-64 "></div>
         
-        <div className="devhub-flex devhub-h-64 devhub-items-center devhub-justify-center">
+        <div className="devhub-flex devhub-h-64 devhub-items-start devhub-justify-center">
         <a href="https://github.com/login/oauth/authorize?client_id=bf37cd3a98ec77d66a8d"> 
         <i className="fa fa-github devhub-text-white " style={logoIcon}></i>
         </a>
@@ -27,7 +27,7 @@ export default function Index() {
       
 
         <div className="devhub-flex devhub-w-screen devhub-justify-left  w-screen devhub-items-end devhub-h-64">
-            <img className="devhub-w-16 devhub--my-24 devhub-p-2 " src="/images/devHub01.png" />
+            <img className="devhub-w-16 devhub-p-2 " src="/images/devHub01.png" />
         </div>
 
         </div>
