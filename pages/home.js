05b5be4f5ae748b45f9a66b2446de1ea23ddc275
Kamogelo <kamogeloCharmaine142@gmail.com>
2020-04-15 08:01:50 +0200

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -9,23 +9,22 @@ export default function Index() {
     <div>
     <Head></Head>
       <div className="devhub-w-screen devhub-h-screen devhub-flex devhub-items-center devhub-justify-center devhub-bg-black">
-        
-        
-        <div className="devhub-flex-wrap">
-          <div className="devhub-flex devhub-h-64 "></div>
-        
-        <div className="devhub-flex devhub-h-64 devhub-items-center devhub-justify-center devhub-text-white">
-            Coming Soon  
-        </div>
-      
 
-        <div className="devhub-flex devhub-w-screen devhub-justify-left  w-screen devhub-items-end devhub-h-64">
-            <img className="devhub-w-16 devhub--my-24 devhub-p-2 " src="/images/devHub01.png" />
+        <div className="devhub-text-white devhub-grid-cols-3" >
+            <div className="devhub-home-text-center">dev</div>
+            
+            <button className="devhub-home-button">Coming soon</button>
         </div>
 
+        <div className="devhub-text-white devhub-grid-cols-3">
+          <div className="devhub-home-text-center">Org</div>
+          <button className="devhub-home-button">Coming soon</button>
         </div>
-  
-
+        <div className="devhub-text-white devhub-grid-cols-3">
+            <div className="devhub-home-text-center">Stack</div>
+            <button className="devhub-home-button">Coming soon</button>
+        </div>
+        
       </div>
       
     
