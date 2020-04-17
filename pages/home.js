19e3b71217cd57a54732c2319ccb49e6318dce67
Kamogelo <kamogeloCharmaine142@gmail.com>
2020-04-17 07:51:38 +0200

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -15,16 +15,16 @@ export default function Index() {
       <div className="devhub-w-screen devhub-h-screen devhub-flex devhub-items-center devhub-justify-center devhub-bg-black">
         
         <div className="devhub-text-white devhub-grid-cols-3" >
-        <div className="devhub-home-text-center"><FontAwesomeIcon icon={faCode} size="50px"/></div>
+        /*<div className="devhub-home-text-center"><FontAwesomeIcon icon={faCode} size="50px"/></div>*/
             <button className="devhub-home-button">Dev</button>
         </div>
 
         <div className="devhub-text-white devhub-grid-cols-3">
-          <div className="devhub-home-text-center"><FontAwesomeIcon icon={faCoffee} /></div>
+          /*<div className="devhub-home-text-center"><FontAwesomeIcon icon={faCoffee} /></div>*/
           <button className="devhub-home-button">Organization</button>
         </div>
         <div className="devhub-text-white devhub-grid-cols-3">
-            <div className="devhub-home-text-center"><FontAwesomeIcon icon={faCode} /></div>
+            /*<div className="devhub-home-text-center"><FontAwesomeIcon icon={faCode} /></div>*/
             <button className="devhub-home-button">Stack</button>
         </div>
         
